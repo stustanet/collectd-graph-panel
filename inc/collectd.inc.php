98cc175bd7c4928f356065ed2911006781b981ec
Wolfgang Walter <wolfgang.walter@stusta.mhn.de>
2013-01-06 01:57:24 +0100

    plugin for ethtstat
    The ethstat-plugin delivers rather different data for different
    for different cards. So display an extra graph for every source.

--- inc/collectd.inc.php
+++ inc/collectd.inc.php
@@ -119,7 +119,7 @@ function group_plugindata($plugindata) {
 	foreach ($plugindata as $item) {
 		# backwards compatibility
 		if ($CONFIG['version'] >= 5 || !preg_match('/^(df|interface)$/', $item['p']))
-			if($item['p'] != 'libvirt')
+			if($item['p'] != 'libvirt' && $item['p'] != 'ethstat')
 				unset($item['ti']);
 		$data[] = $item;
 	}
